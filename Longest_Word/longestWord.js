3949f856006da92a2ec4142fea4969c330d8986d
Guilherme Guimaraes <Technestowl@gmail.com>
2023-12-08 09:39:09 -0500

    layed down basic structure of my functions

---
+++ Longest_Word/longestWord.js
@@ -0,0 +1,42 @@
+// Given an array of marvel heros, write a function that accepts the array of names. The function should return the hero with the longest name.
+
+
+const marvelHeros = [
+    "Ant-man",
+    "Black Panther",
+    "Black Widow",
+    "Captain America",
+    "Doctory Strange",
+    "Hawkeye",
+    "Hulk",
+    "Iron Man",
+    "Luke Cage",
+    "Moon Knight",
+    "Mrs.Marvel",
+    "Scarlet Witch" ,
+    "Spder-Man",
+    "Thor",
+    "Wasp",
+    "Blue Bettle"
+]
+
+//driver function, used to display and passing values
+function findHero(){
+    
+    let lWord = findLongestName(marvelHeros);
+
+    //display in HTML
+    document.getElementById("results").innerHTML = lWord;
+}
+
+
+
+
+function findLongestName(arrayTBsorted){
+   
+    let longestName = "";
+
+   
+   
+    return longestName
+}

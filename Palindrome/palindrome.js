7aacce6a9b9352d37f76dd52188b8e48fddb897c
Guilherme Guimaraes <Technestowl@gmail.com>
2023-12-11 18:04:55 -0500

    finsihed logic

--- Palindrome/palindrome.js
+++ Palindrome/palindrome.js
@@ -1,17 +1,43 @@
 //my paldindrom function needs to return a boolean value.
 // I need to split the given input into an array of strings
 // The, I must sift out all special charcters or spaces, and set each idex item to lowercase. 
+// document.getElementById("submitBtn").addEventLister("click", displayResult())
 
 
-let resultBox = document.getElementById("result").innerHTML;
 
-function palindromeCheck(){
-    let input = document.getElementbyId("input1").value;
+
+function displayResult(){
+    let str = document.getElementById("input1").value;
+
+
+    if(str == ""){
+        alert("enter a word or phrase in the input field")
+        return;
+    }
+
+    let plaindrome = palindromeCheck(str);
+
+    let msg = "";
+
+    if(plaindrome == true){
+        msg = "The phrase is a palindrome"
+    } else{
+        msg = "The phrase is not a plaindrome"
+    }
+
+    document.getElementById("result").innerHTML = msg;
+
+
+}
+
+document.getElementById("btnSubmit").addEventListener("click",displayResult);
+
+
+
+
+
+function palindromeCheck(str){
    
-    // if(input == ""){
-    //     alert("please enter a word or phrase")
-    // }
-
     //remove special characters or spaces
     let cleanString = str.replace(/[^A-Z0-9]/ig, "");
 
@@ -24,12 +50,15 @@ function palindromeCheck(){
     for(let index = cleanString.length - 1; index >= 0; index--){
         reversedStr += cleanString[index];
     }
+
+
     // check to see if is a plaindrome
     if(cleanString == reversedStr){
-        resultBox = "Yes this is a palindrom"
         return true;
     }else {
         return false;
     }
 
 }
+
+

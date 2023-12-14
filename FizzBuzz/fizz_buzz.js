0aeebeaa688187235f34dd0c5def4a45ee354784
Guilherme Guimaraes <Technestowl@gmail.com>
2023-12-14 14:32:09 -0500

    refined fizzbuzz logic

--- FizzBuzz/fizz_buzz.js
+++ FizzBuzz/fizz_buzz.js
@@ -13,14 +13,23 @@
 
 
 for(let i = 1; i <= 30; i++){
-    if(i % 3 == 0 && i % 5 == 0){
-        console.log("FizzBuzz")
-    }else if( i % 3 == 0 ){
-        console.log("fizz")
-    }else if(i % 5 == 0  ){
-        console.log("buzz")
-    }else {console.log(i)}
 
+// first attempt, works but seems clunky. 
 
+    // if(i % 3 == 0 && i % 5 == 0){
+    //     console.log("FizzBuzz")
+    // }else if( i % 3 == 0 ){
+    //     console.log("fizz")
+    // }else if(i % 5 == 0  ){
+    //     console.log("buzz")
+    // }else {console.log(i)}
 
+//more concise method. This allows me to change future itterations of "fizz | buzz" more easily.
+    let output = "";
+
+    if(i % 3 == 0){output += "Fizz"}
+    if(i % 5 == 0){output += "Buzz"}
+    if (output == ""){output = i}
+
+    console.log(output)
 };

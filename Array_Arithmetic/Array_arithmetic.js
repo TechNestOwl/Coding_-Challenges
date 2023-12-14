5785458c6cb1ac029f7f6dca1ee1d50526bc567c
Guilherme Guimaraes <Technestowl@gmail.com>
2023-12-14 17:13:51 -0500

    laying out framework and styles for challenge page

--- Array_Arithmetic/Array_arithmetic.js
+++ Array_Arithmetic/Array_arithmetic.js
@@ -2,6 +2,11 @@
 //this function returns the sum of all numbers. 
 
 
+
+
+
+
+
 let arr1 = [8,2,4,29,15,-5,6,88];
 
 function arrSum(arr){
@@ -14,5 +19,37 @@ function arrSum(arr){
     // console.log(`The sum of numbers ${arr} is ${sum}`)
 }
 
-arrSum(arr1)
 
+function arrAvg(arr){
+    let average = 0;
+    let sum = 0;
+    
+    for(let i = 0; i < arr.length; i++){
+        sum += arr[i];
+    };
+    
+    average = (sum / arr.length)
+
+    return average;
+}
+
+
+
+function arrMax(arr){
+    let max = arr[0];
+
+    for(let i = 1; i < arr.length; i++){
+        if(arr[i] > max){max = arr[i]};
+    }
+    return max;
+}
+//using for loop instead of Math.Max() because it scales better with a larger array. 
+
+function arrMin(arr){
+    let min = arr[0];
+
+    for(let i = 1; i < arr.length; i++){
+        if(arr[i]< min){min = arr[i]}
+    }
+    return min;
+}

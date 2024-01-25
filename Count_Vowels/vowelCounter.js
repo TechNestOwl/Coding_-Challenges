116f6c63d572a7418578cf98e8f1a10eb141b1d0
Guilherme Guimaraes <Technestowl@gmail.com>
2024-01-25 17:26:55 -0500

    solved challenge

--- Count_Vowels/vowelCounter.js
+++ Count_Vowels/vowelCounter.js
@@ -1,10 +1,10 @@
 /*
     Given a string write a function that counts the vowels in the string.
-    The function should return an integer of the vowel count. 
-    Display a message indicating the result of the function. 
+    The function should return an integer of the vowel count.
+    Display a message indicating the result of the function.
     Ignore "Y"
 
-    Exrta credit: have your function return the Vowel count and the vowels found. 
+    Exrta credit: have your function return the Vowel count and the vowels found.
 */
 
 
@@ -13,38 +13,30 @@ let string1 = "I would like to know how many vowels are in this string";
 
 function vowelFinder(string){
 
-    let input = string.toLowerCase();
     let vowels = ["a","e","i","o","u"];
     let foundVowels = [];
     let vowelCounter = 0;
+    let input = string.toLowerCase().split("");
 
-
+   
     function comaparison(vowel){
-        
+
         input.forEach((e)=>{
             if(e == vowel){
-                return true
-            }else{ return false }
+                foundVowels += e; //?
+                vowelCounter ++;
+            }
         })
-
     };
-
-
-
+    
     vowels.forEach((e) => {
-        if(comaparison(e) ){
-
-            foundVowels += e; //?
-            vowelCounter ++;
+        comaparison(e)
+    })
 
 
-        }
-    })    
+    let output = `Found ${vowelCounter} vowels, which are ${foundVowels}`;
 
-    let output = `Found ${vowelCounter}, which are ${foundVowels}`;
-
-
-    console.log(output)
+    console.log(output);
     return output;
 }
 

f8ae1e7005f7e286d5b68f9ad526f822e96280ce
Guilherme Guimaraes <Technestowl@gmail.com>
2024-02-10 16:34:25 -0500

    finished bracket exercise

--- Balanced_Brackets/balancedB2.js
+++ Balanced_Brackets/balancedB2.js
@@ -1,36 +1,73 @@
 
 const testBrackets1 = "[{(())}]"; // true
-const testbrackets2 = "[()]{}{[()()]()}"; // true
+const testBrackets2 = "[()]{}{[()()]()}"; // true
 const testBrackets3 = "[{((}))}]"; // false
+const testBrackets4 = "[{{((}))}]"; // false
 
 
 
 function isBalanced(caseTest){
-    var x;
-    
-    if(caseTest == x){
-        return true
-    }else return false
+   
+    let stack = [];
+
+    for(let i = 0; i < caseTest.length; i++){
+        
+        let item = caseTest[i];
+
+        if(item == '(' || item == '[' || item == '{'){
+
+            stack.push(item);
+
+        }else if(item == ')' || item == ']' || item == '}'){
+
+            check = stack.pop();
+            switch(item){
+                case ')':
+                    if(check != '('){
+                        return false
+                    }
+                    break;
+                case '}':
+                    if (check != '{'){
+                        return false
+                    }
+                    break;
+                case ']':
+                    if (check != '['){
+                        return false
+                    }
+                    break;
+            };
+        
+        };
+    };
+    return stack.length == 0;
 };
 
 
 
 
 
-function checkBrackets(input){
+function bracketResults(input){
 
     let output = {
         bracket: input,
         result: false,
+        msg: "",
     };
 
+    let balanced = isBalanced(input);
 
-    if(isBalanced(input)){
-        output.result = true
+
+    if(balanced == true){
+        output.result = true;
+        output.msg = "This bracket set is balanced";
+    }else{
+        output.msg = "This bracket set is unbalanced";
     };
 
     console.log(output);
     return output;
 };
 
-checkBrackets(testBrackets1);
+bracketResults(testBrackets4);

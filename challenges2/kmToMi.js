405d4d7761aeb990bcb8e4ef78b31d83f3354370
Guilherme Guimaraes <Technestowl@gmail.com>
2024-02-20 11:40:44 -0500

    km to mi conversion

---
+++ challenges2/kmToMi.js
@@ -0,0 +1,13 @@
+// write a function that takes a number param in km converts it to miles. Return the converted number.
+
+
+function convertKmMi(km){
+
+   let convertedMi = km * .621;
+   console.log(convertedMi);
+
+   return convertedMi;
+};
+
+
+convertKmMi(100);
--- challenges2/longestWord.js
+++ challenges2/longestWord.js
@@ -1,7 +1,6 @@
 // write a function that returns the longest string in a given array
 
 
-
 const marvelHeroes = [
     "Ant-Man",
     "Black Panther",
@@ -21,18 +20,34 @@ const marvelHeroes = [
 ];
 
 
-// returning longest string with for loop
+// function longestStr(array){
+//     var longestName = ['test1'];
+
+//     for (let index = 0; index < array.length; index++) {
+//         const name = array[index];
+//         let testCase = longestName[0];
+
+        
+//         if(name.length > testCase.length){
+//             longestName.pop();
+//             longestName.push(name)
+//         };
+        
+//     }
+//     console.log(longestName)
+//     return longestName;
+// };
+
+// more efficient method, returning a simple string.
 function longestStr(array){
-    var longestName = ['test1'];
+    var longestName = "";
 
     for (let index = 0; index < array.length; index++) {
         const name = array[index];
-        let testCase = longestName[0];
 
         
-        if(name.length > testCase.length){
-            longestName.pop();
-            longestName.push(name)
+        if(name.length > longestName.length){
+           longestName = name;
         };
         
     }
